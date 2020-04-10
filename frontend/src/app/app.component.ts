3cacd00b814b4516b232923fee46e8d2865f0ed5
Anthony Joaquin Bravo Berrio <anthony.bravo.berrio@gmail.com>
2020-04-10 17:29:34 -0500

    cambios de cache

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+
+  title: String = 'test';
+
+  ngOnInit() {
+    this.getHeartBeat();
+  }
+
+
+  getHeartBeat() {
+
+    window.fetch('/heartbeat')
+      .then(function (response) {
+        console.log(response)
+        return response.text();
+      })
+      .then((myJson) => {
+        console.log(this)
+        this.title = myJson;
+        
+      }).catch(() => null);
+  }
+
+
+
+
 }

4cdea2f39deefbd0381bd088f8ca48c518d38c8e
Anthony Joaquin Bravo Berrio <anthony.bravo.berrio@gmail.com>
2020-04-09 19:22:04 -0500

    Se configura proyecto con heartbeat y travis.yml inicial

---
+++ test-backend/src/main/java/com/test/controller/HeartbeatController.java
@@ -0,0 +1,18 @@
+package com.test.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/heartbeat")
+public class HeartbeatController {
+
+	@GetMapping()
+	public ResponseEntity<String> hearbeat() {
+
+		return ResponseEntity.ok("PUM PUM");
+
+	}
+}
